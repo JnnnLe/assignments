3a47ab5b1748e255bd5c27a15560c1d5068dcedd
JnnnLe <jnnnle.sf@gmail.com>
2019-07-25 15:01:00 -0700

    Complete movie function

--- week3/js3.js
+++ week3/js3.js
@@ -7,3 +7,40 @@ const getGrade = score => {
   if (5 < score && score <= 10) { return `E` }
   if (0 <= score && score <= 5) { return `F` }
 };
+
+// planning on going to the movies with friends who are not really friends, get the popcorn ready
+const movieNight = friendsArr => {
+  let going = [];
+
+  for (let friend of friendsArr) {
+    while (going.length <= 3) {
+
+      if (going.length == 2) {
+        going.push(`B`);
+        return going;
+      }
+
+      if (friend === `D`) {
+        if (going.length < 2 && going[0] !== 'M') {
+        going.push(`D`, `A`);
+        }
+      }
+
+      if (friend == `P` || friend == `M`) {
+        if (friend.length == 1) {
+          if (going.length === 1 && going[0] !== `D`) {
+            going.push(`P`, `M`);
+            return going;
+          } 
+        }
+
+        if (going.length === 0) {
+          going.push(`P`, `M`, `B`);
+          return going;
+        }
+      }
+    }
+  }
+
+  return going;
+};
