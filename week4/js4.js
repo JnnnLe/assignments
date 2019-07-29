3995053a4aada9f82a95fdadb3086d4d58ceb343
JnnnLe <jnnnle.sf@gmail.com>
2019-07-29 09:28:56 -0700

    Print person to places

--- week4/js4.js
+++ week4/js4.js
@@ -1,12 +1,23 @@
 const printVacations = arr => {
-  let locations = ''; 
+  let locations = ``; 
   for (let i = 0; i < arr.length; i++) {
     for (let j = 1; j < arr[i].length; j++) {
       locations = locations + arr[i][j];
     }
-    console.log(`${arr[i][0]} really wants to go to ${locations}`);
+    console.log(`${arr[i][0]} really wants to go to ${locations}.`);
   }
 };
 
 printVacations([ ['Tammy', 'Tahiti'], ['Erin', 'Banff, Alberta, Canada'], ['Janet', 'London']]);
 
+const moreVacations = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    let locations = ``;
+    for (let j = 0; j < arr[i][1].length; j++) {
+      locations = locations + arr[i][1][j];
+    };
+    console.log(`${arr[i][0]} is willing to go to ${locations}.`)
+  }
+};
+
+moreVacations([ ['Tammy', ['Tahiti', 'Bali', 'Hawaii']], ['Erin', ['Banff, Alberta, Canada', 'Iceland']], ['Janet', ['London', 'Hogwarts']] ]);
