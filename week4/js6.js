b13fcfeb06717b1542622158f019d3531cca9f3a
JnnnLe <jnnnle.sf@gmail.com>
2019-07-30 09:59:51 -0700

    Answer Objective Qs for objects

---
+++ week4/js6.js
@@ -0,0 +1,21 @@
+// Write down the syntax for declaring an Object.
+  let j = {};
+  let jk = new Object(); // Not preferred but legal actionin programming
+
+// What do you mean by Complex Objects? Give an example.
+  // Complex objects occur when an object contains more than one data type, and when a variable conatins more than one object.
+  
+
+// Explain some built-in Object Methods in JavaScript.
+  // .hasOwnProperty => returns true or false to a certain key in the object
+  // .keys => return an array of all the keys in object
+  // .entires => returns an array fo keys and values
+
+// What is a property of an Object?
+  // Property of an object refers to the key in a key-value pair of an object
+
+// Differentiate between dot and bracket notation.
+  // They create the same result(s) it is a matter of preference
+
+// How do you access nested object inside an array?
+  // Each element pf the list is an object, one must index into the list to access the object(s) then use the property names to access the object's attribute
