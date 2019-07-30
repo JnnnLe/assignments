7b07bd7f3deb29453fcffdbc2a7a6802f00eea58
JnnnLe <jnnnle.sf@gmail.com>
2019-07-30 10:46:57 -0700

    Write two tests, where one is to help improve fn

---
+++ week4/tdd.js
@@ -0,0 +1,20 @@
+// Independent Practice
+var countWords = function(sentence) {
+  return sentence.split(" ").length;
+};
+
+describe("countWords", function() {
+  it("should count a single letter", function() {
+    expect(countWords("a")).toBe(1);
+  });
+
+  it("it should count words", function() {
+    expect(countWords("Hi Leah, thanks for your feedback!")).toBe(6);
+  });
+
+  // challenge: Try to find some input where the fn gives the wrong result
+  it("it should ignore punctuations", function() {
+    expect(countWords("!!#$%%$#.")).toBe(0); 
+    // now the programmar must go back to modify their function tofilter out punctuations because puncutations !== words
+  })
+});
