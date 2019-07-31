386bb584dc0e222a1742a3ce16289533ba379882
JnnnLe <jnnnle.sf@gmail.com>
2019-07-31 10:23:05 -0700

    Check for understanding

--- week4/tdd.js
+++ week4/tdd.js
@@ -39,3 +39,27 @@ const firstNonRepeatingLetter = str => {
 
 
 // Challenge: Ping Pong Pairing
+
+
+
+
+// Checking for understanding:
+// What is an automated test?
+  // Automated testing differs from manual testing where a human being is responsible for single-handedly testing the functionality of the software in the way a user would. Because automated testing is done through an automation tool, less time is needed in exploratory tests and more time is needed in maintaining test scripts while increasing overall test coverage.
+
+
+// What is TDD?
+  // A software development process that relies on the repetition of a very short development cycle: requirements are turned into very specific test cases, then the software is improved so that the tests pass. This is opposed to software development that allows software to be added that is not proven to meet requirements.
+
+// What are some advantages of TDD?
+  // Ensures your code works as expected. It protects against future problems, it helps developers foresee the ways the a user will use or abuse their product.
+
+// What are some disadvantages of TDD?
+  // Time consuming, often tests will exceed the lines of code written for the function/component/feature
+  // Users don't care for testing unless a feature is seriously broken
+
+// Where should your tests live in your project?
+  // In it's own directory along wit
+
+// How do you run your tests?
+  // One must write them.
