baaf9ebcfd806cd316adfe7afb0efa243f0fedda
JnnnLe <jnnnle.sf@gmail.com>
2019-08-01 16:02:49 -0700

    Time complexity PT. 3

--- week4/bigO.js
+++ week4/bigO.js
@@ -236,4 +236,55 @@ const countOccurencesC = (array) => {
     counts[item] += 1;
   }
   return counts;
-}
+};
+
+
+//=========================================PART THREE=========================================
+/*
+Runtime Exercise 3: Designing algorithms and writing code
+How would you solve the following problems? Can you think of an O(n^2), O(n log n), and O(n) solution to each problem? If you have time, code one of the functions you thought of to solve the problem.
+
+Find the integer that occurs most frequently in an array
+Examples:
+
+[1,4,5,4,2,2,4] returns 4
+Time Complexity: O(n)
+
+
+Write a function that takes an array of numbers and returns the greatest difference you can get by subtracting any two of those numbers.
+Examples:
+
+[1, 5, 3, 1, 15] returns 14
+Time Complexity: O(n^2)
+
+
+Find the only element in an array that only occurs once
+Examples:
+
+[3,5,3,4,6,6,4] returns 5
+Time Complexity: O(n2)
+
+
+Find the common elements of 2 integer arrays
+Examples:
+
+([1,3,2,5], [3,9,8,1]) returns [1, 3]
+Time Complexity: O(n^2)
+
+
+Determine if 2 Strings are anagrams (use the same letters re-arranged)
+Examples:
+
+("cat", "act") returns true
+("at", "tat") returns false
+("cat", "dog") returns false
+Time Complexity: O(n^2)
+
+
+Check if a String is composed of all unique characters
+Examples:
+
+"banana" returns false
+"bacon" returns true
+Time Complexity: O(n)
+*/
