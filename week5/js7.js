ce48ad681cd205a447a7deb3d94f484e7a855f34
JnnnLe <jnnnle.sf@gmail.com>
2019-08-07 13:58:25 -0700

    OOP

---
+++ week5/js7.js
@@ -0,0 +1,18 @@
+/*
+Find definition of following terms on your own:
+
+Namespace - creates an awareness for the function's scope of method and value.
+
+Hoisting - The way functions and varibles are defined will determine where they get places in the heiarchy of the program.
+
+Arity or function.prototype.length - property indicates the number of parameters expected by the function.
+
+Anonymous - Anonymous functions are functions that have no name and usually written to do a small/simple task then no longer needed.
+
+Closure - the combination of a function and the lexical environment within which that function was declared.
+
+Currying - is the process of taking a function with multiple arguments and returning a series of functions that take one argument and eventually resolve to a value. 
+
+Pragma - an interpreter that forces the modern interpreter to behave a certain way, but without breaking older interpreters.
+eg. My mother, growing up she had to uphold a traditional way of raising my siblings and me in order to make my grandmother happy. And she had to do this without compromising our American upbring, in the end it made us better rounded Asian American children, now adults.
+*/
