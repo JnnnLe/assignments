5c7386a738c88ce56e3557553ab6c118ce43ab9c
JnnnLe <jnnnle.sf@gmail.com>
2019-08-21 11:25:17 -0700

    Require queries

--- month2/week7/node-api-postgres/index.js
+++ month2/week7/node-api-postgres/index.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+const db = require('./queries');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
