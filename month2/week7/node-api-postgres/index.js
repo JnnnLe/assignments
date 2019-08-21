009a7b823f59dc95bf7a8424877357bb295badc6
JnnnLe <jnnnle.sf@gmail.com>
2019-08-21 10:59:33 -0700

    Create routes for CRUD application

--- month2/week7/node-api-postgres/index.js
+++ month2/week7/node-api-postgres/index.js
@@ -15,6 +15,28 @@ app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 });
 
+// creating all the routes for CRUD application
+app.get('/users', (request, response) => {
+
+});
+
+app.get('/users/:id', (request, response) => {
+
+});
+
+app.post('users', (request, response) => {
+
+});
+
+app.put('/users/:id', (request, response) => {
+
+});
+
+app.delete('/users/:id', (request, response) => {
+
+});
+
+
 // app listen on port 3000
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
