3b79b47a1e83e1aa1d1e8b3702a7d614a3761050
JnnnLe <jnnnle.sf@gmail.com>
2019-08-21 11:21:13 -0700

    DELETE a user

--- month2/week7/node-api-postgres/queries.js
+++ month2/week7/node-api-postgres/queries.js
@@ -54,3 +54,15 @@ const updateUser = (request, response) => {
     }
   )
 };
+
+// DELETE a user
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`User deleted with ID: ${id}`);
+  })
+};
