b1bb8f3bc2d6db5ab77f31feaf89d23694b0fbe0
JnnnLe <jnnnle.sf@gmail.com>
2019-08-21 11:19:10 -0700

    PUT update data in an exisiting user

--- month2/week7/node-api-postgres/queries.js
+++ month2/week7/node-api-postgres/queries.js
@@ -39,3 +39,18 @@ const createUser = (request, response) => {
     response.status(200).send(`User added with id: ${results.insertId}`);
   })
 };
+
+// PUT updates data in an existing user
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { name, email } = request.body;
+
+  pool.query(
+    'UPDATE user SET name = $1, email = $2 WHERE id = $3', [name, email, id], (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`Users modified with ID: ${id}`);
+    }
+  )
+};
