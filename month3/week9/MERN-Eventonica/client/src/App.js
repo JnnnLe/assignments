65d34fc8b9a60f99954dbf2e09c6c54e90e178b6
JnnnLe <jnnnle.sf@gmail.com>
2019-09-01 11:10:08 -0700

    Search API and hide keys

--- month3/week9/MERN-Eventonica/client/src/App.js
+++ month3/week9/MERN-Eventonica/client/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import keys from './keys'
+import AddUser from './components/AddUser/AddUser';
+
+class App extends React.Component {
+  state = {
+    input: ''
+  };
+
+  handleOnChange = e => {
+    let input = e.target.value;
+    this.setState({ input });
+    console.log(keys)
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    const URL = `https://rest.bandsintown.com/artists/${this.state.input}/events?app_id=${keys.BANDSINTOWN_API_KEY}`
+    axios.get(URL)
+    .then(res => console.log(res))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Eventonica</h1>
+        <form onSubmit={this.handleSubmit}>
+        <input onChange={this.handleOnChange} />
+        </form>
+  
+        <AddUser />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ month3/week9/MERN-Eventonica/client/src/components/AddUser/AddUser.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AddUser extends React.Component {
+  state = {
+
+  }
+  render() {
+    return (
+      <div>Add a user:</div>
+    )
+  }
+}
+
+export default AddUser;
