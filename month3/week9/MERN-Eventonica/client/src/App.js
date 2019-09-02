e08b1f47739d97977af76d85decc6f86e4e5b48c
JnnnLe <jnnnle.sf@gmail.com>
2019-09-01 18:27:29 -0700

    Display concert and used short circuit operator instead of async

--- month3/week9/MERN-Eventonica/client/src/App.js
+++ month3/week9/MERN-Eventonica/client/src/App.js
@@ -7,19 +7,33 @@ import AddUser from './components/AddUser/AddUser';
 
 class App extends React.Component {
   state = {
-    input: ''
+    input: '',
+    data: null
   };
 
   handleOnChange = e => {
     let input = e.target.value;
     this.setState({ input });
-    console.log(keys)
-  }
+  }; 
+
   handleSubmit = e => {
     e.preventDefault();
     const URL = `https://rest.bandsintown.com/artists/${this.state.input}/events?app_id=${keys.BANDSINTOWN_API_KEY}`
     axios.get(URL)
-    .then(res => console.log(res))
+    .then(res => {
+      const { data, status } = res;
+      if (status == 200) {
+        this.setState({ data });
+      }
+    })
+  };
+
+  displayConcerts = () => {
+    return this.state.data.map(concert => {
+      return (
+        <h1>{concert.venue.name}</h1>
+      )
+    })
   }
 
   render() {
@@ -29,7 +43,7 @@ class App extends React.Component {
         <form onSubmit={this.handleSubmit}>
         <input onChange={this.handleOnChange} />
         </form>
-  
+        {this.state.data !== null && this.displayConcerts()}
         <AddUser />
       </div>
     );
