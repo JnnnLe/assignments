8a1068a555976b6cbc403e386e041c14bae2b0b8
JnnnLe <jnnnle.sf@gmail.com>
2019-09-04 21:17:05 -0700

    Use tilda instead of quotes

--- month3/week9/MERN-Eventonica/models/User.js
+++ month3/week9/MERN-Eventonica/models/User.js
@@ -7,6 +7,6 @@ const userSchema = new Schema ({
   concerts: Array
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model(`User`, userSchema);
 
 module.exports = User;
