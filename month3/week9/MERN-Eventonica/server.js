a4ef160f964627f10d3e21ed47f3b115713ff2cf
JnnnLe <jnnnle.sf@gmail.com>
2019-09-04 21:16:17 -0700

    Reflect data in db

--- month3/week9/MERN-Eventonica/server.js
+++ month3/week9/MERN-Eventonica/server.js
@@ -1,82 +1,90 @@
-const express = require('express');
-const axios = require('axios');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+require(`dotenv`).config();
+const express = require(`express`);
+const axios = require(`axios`);
+const mongoose = require(`mongoose`);
+// const bodyParser = require(`body-parser`);
 
 const app = express();
 const PORT = 8080;
-require('dotenv').config();
+// app.use(bodyParser());
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-hw6jk.mongodb.net/test?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  console.log('Open connection to MongoDB!')
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-hw6jk.mongodb.net/test?retryWrites=true&w=majority
+`, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on(`error`, console.error.bind(console, `connection error:`));
+db.once(`open`, () => {
+  console.log(`Connected!`)
 });
 
 const Concert = require(`./models/Concert`);
 const User = require(`./models/User`);
 
-app.use(bodyParser());
-
 // home
-app.get('/', (req, res) => {
-  res.redirect('http://localhost:3000');
+app.get(`/`, (req, res) => {
+  res.redirect(`http://localhost:3000`);
 });
 
 // add new user to db
-app.post('/api/addUser', (req, res) => {
+app.post(`/api/addUser`, (req, res) => {
   const { user_name } = req.body;
-  let newUser = new User({
-    user_name
-  })
-  newUser.save((err) => {
+    User.create({user_name}, (err, response) => {
     if (err) return handleError(err);
+    console.log(response);
+    res.json(response);
   })
-  res.send(`Saved new user: ${newUser.user_name}!`)
 });
 
-
 // add new concert to db
-app.post('/api/addConcert', (req, res) => {
-  const { artist_name, date_time, location_name, location } = req.body;
+app.post(`/api/addConcert`, (req, res) => {
+  const { artist_name, date_time, location_name, location, attendees } = req.body;
   let newConcert = new Concert({
     artist_name,
     date_time,
     location_name,
-    location
+    location,
+    attendees
   })
   newConcert.save((err) => {
     if (err) return handleError(err);
   })
-  res.send(`Saved new concert: ${newConcert.artist_name}!`)
+  res.send(`Saved new concert: ${newConcert,artist_name}!`)
 });
 
-// find user in db
-
+// find user in db by given id
+app.get(`/api/users/:user_name`, (req, res) => {
+  const { id } = req.params;
+  // TODO: find user by id
+  User.find({user_name: id}, (err, users) => {
+    if (err) throw err;
+    res.send(users);
+  })
+});
 
 
 // find concert in db by given id
-app.get('/api/findConcert', (req, res) => {
+app.get(`/api/findConcert`, (req, res) => {
   Concert.find({}, (err, concerts) => {
-    console.log('Return all concerts')
+    if (err) {
+      console.log(err);
+    }
+    console.log(`Found stuff`)
     res.json(concerts);
   })
 });
+
+// delete user in db given id
+
+// delete concert in db given id
+
+// Search all of the concerts a specific user is attending
+
+// Search all of the users going to a specific concert
 
 
 // express server
 app.listen(PORT, () => {
   console.log(`App is listening on ${PORT}.`)
 });
-
-
-// once cluster is made, use mongoose to establish the connection (eg. mongoose.connect look at doc)
-// click button on cluster for uri 
-  // mongodb+srv://jennifer:<password>@cluster0-hw6jk.mongodb.net/test?retryWrites=true&w=majority
-// keep in mind everything revolves around models
-// create models dir in root for schemas (schema doc for mongoose)
-// in the routes, 
