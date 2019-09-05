f19999b60b4b3956327a2348deade722f79ac743
JnnnLe <jnnnle.sf@gmail.com>
2019-09-04 18:55:16 -0700

    Edit Concert model

--- month3/week9/MERN-Eventonica/models/Concert.js
+++ month3/week9/MERN-Eventonica/models/Concert.js
@@ -3,10 +3,11 @@ const mongoose = require(`mongoose`);
 const Schema = mongoose.Schema;
 
 const concertSchema = new Schema ({
-  artistName: String,
+  artist_name: String,
   date_time: String,
   location_name: String,
-  location: String
+  location: String,
+  attendees: Array
 });
 
 const Concert = mongoose.model('Concert', concertSchema);
